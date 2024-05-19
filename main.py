379b7ab50d2d65bc74190e3f008b328eaa87986a
HajrahSiddiqui <167708894+HajrahSiddiqui@users.noreply.github.com>
2024-05-19 07:19:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from playsound import playsound
 from tkinter import *
 from tkinter import messagebox
+import tkinter as tk
+from PIL import Image, ImageTk
+from captcha.image import ImageCaptcha
 import requests
 import threading
+
 def printit():
   receiver = "A"
   threading.Timer(0.5, printit).start()
@@ -13,33 +17,34 @@ def printit():
   print(screams.json())
 printit()
 
-#python3 -m venv myenv
-#source myenv/bin/activate
+# Create the main window
+parent = tk.Tk()
+parent.title("Image in Tkinter")
 
-# for playing note.wav file
-#def show():
- #   messagebox.showinfo("Test Popup", "Hello world")
-#root = Tk()
-#root.title("Main Window")
-#root.geometry("500x500")
-#toplevel = Toplevel(root)
-#toplevel.title("Toplevel Window")
-#toplevel.geometry("300x300")
-#show_button = Button(root , text = "Show popup" , command = show)
-#show_button.place(x = 200 , y = 200)
-mainloop()
+# Load and display an image 
+#(replace 'your_logo.png' with the path to your image file)
+image = Image.open('stuff.png')
+image = ImageTk.PhotoImage(image)
 
-main = Tk()
-photo = PhotoImage(file='/Users/hajrahsiddiqui/Documents/hackathon 2024 project /zombie image .webp')
-main.iconphoto()
-main.geometry("1000000x1000000")
-main.mainloop()
+# Create a label to display the image
+image_label = tk.Label(parent, image=image)
+image_label.pack()
 
-#COME BACK TO WINSOUND THING
-#---
-#winsound.playsound("zombie.mp3", winsound.SND_ASYNC + winsound.SND_LOOP)
-#input("Enter any input to stop sound")
-#winsound.playsound(None, 0)
-#input("Enter any input to playm sound")
-#winsound.PlaySound("zombie.mp3", winsound.SND_ASYNC + winsound.SND_LOOP)
-#input("Enter any input to exit")
+# Start the Tkinter event loop
+parent.mainloop()
+
+
+#main = Tk()
+#photo = PhotoImage(file='/Users/hajrahsiddiqui/Documents/hackathon 2024 project /zombie image .webp')
+    # Select the Imagename  from a folder 
+    # opens the image
+#img = Image.open(photo)
+
+    # resize the image and apply a high-quality down sampling filter
+#img = img.resize((1000000, 1000000), Image.LANCZOS)
+    
+#img.show() 
+#main.geometry("1000000x1000000")
+
+    
+#main.mainloop()
